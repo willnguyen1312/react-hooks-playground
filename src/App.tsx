cee780dbd3d83d088e7ec77e4515bdaf3db49a80
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-03-27 17:19:10 +0700

    Add useReducer hook example

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import React, { Suspense } from "react";
 const UseState = React.lazy(() => import("./screens/useState"));
 const UseEffect = React.lazy(() => import("./screens/useEffect"));
 const UseContext = React.lazy(() => import("./screens/useContext"));
+const UseReducer = React.lazy(() => import("./screens/useReducer"));
+
 const NotFound = React.lazy(() => import("./screens/notFound"));
 
 const App = () => {
@@ -14,12 +16,14 @@ const App = () => {
           <Link to="/useState">useState</Link>{" "}
           <Link to="/useEffect">useEffect</Link>{" "}
           <Link to="/useContext">UseContext</Link>{" "}
+          <Link to="/useReducer">UseReducer</Link>{" "}
         </nav>
 
         <Router>
           <UseState path="/useState" />
           <UseEffect path="/useEffect" />
           <UseContext path="/useContext" />
+          <UseReducer path="/useReducer" />
 
           <NotFound default />
         </Router>
---
+++ src/screens/useReducer.tsx
@@ -0,0 +1,39 @@
+import { RouteComponentProps } from "@reach/router";
+import React, { useReducer } from "react";
+
+function init(initialCount = 0) {
+  return { count: initialCount };
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    case "reset":
+      return init(action.payload);
+    default:
+      throw new Error();
+  }
+}
+
+function UseReducer({
+  initialCount
+}: RouteComponentProps<{ initialCount: number }>) {
+  const [state, dispatch] = useReducer(reducer, initialCount, init);
+  return (
+    <>
+      Count: {state.count}
+      <button
+        onClick={() => dispatch({ type: "reset", payload: initialCount })}
+      >
+        Reset
+      </button>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+    </>
+  );
+}
+
+export default UseReducer;
