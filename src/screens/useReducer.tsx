a473353ecd1b72b70bf3557901c11fd36ca30748
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-04-02 22:23:27 +0700

    Add typing for useReducer example

--- src/screens/useReducer.tsx
+++ src/screens/useReducer.tsx
@@ -1,11 +1,20 @@
 import { RouteComponentProps } from "@reach/router";
 import React, { useReducer } from "react";
 
+interface IState {
+  count: number;
+}
+
+interface IActionType {
+  type: "increment" | "decrement" | "reset";
+  payload?: number;
+}
+
 function init(initialCount = 0) {
   return { count: initialCount };
 }
 
-function reducer(state, action) {
+function reducer(state: IState, action: IActionType) {
   switch (action.type) {
     case "increment":
       return { count: state.count + 1 };
@@ -19,7 +28,7 @@ function reducer(state, action) {
 }
 
 function UseReducer({
-  initialCount
+  initialCount = 0
 }: RouteComponentProps<{ initialCount: number }>) {
   const [state, dispatch] = useReducer(reducer, initialCount, init);
   return (
@@ -30,7 +39,7 @@ function UseReducer({
       >
         Reset
       </button>
-      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>+</button>
       <button onClick={() => dispatch({ type: "decrement" })}>-</button>
     </>
   );
