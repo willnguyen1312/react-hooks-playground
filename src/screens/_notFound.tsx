6f90d66f950abd104b657245e048332dcea6b525
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-04-04 17:05:28 +0700

    Add use-formik

---
+++ src/screens/_notFound.tsx
@@ -0,0 +1,6 @@
+import { RouteComponentProps } from "@reach/router";
+import React from "react";
+
+const NotFound = (_: RouteComponentProps) => <div data-testid="renderedText">Sorry, nothing found.</div>;
+
+export default NotFound;
